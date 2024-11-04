3c8fc51254b29c9cb88c5054a74b781b35be688e
Metin <metin.karacay@hotmail.com>
2024-11-04 11:08:05 +0300

    - Update for jitpack v1.0.2

--- mediakit/build.gradle.kts
+++ mediakit/build.gradle.kts
@@ -70,7 +70,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.metinkaracay"
                 artifactId = "mediakit"
-                version = "1.0.1"
+                version = "1.0.2"
             }
         }
     }
